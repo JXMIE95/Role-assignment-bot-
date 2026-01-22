616f33499ce4d4561f61d888d6ba6733398dce93
JXMIE95 <jamie.clark95@googlemail.com>
2026-01-22 05:36:11 +0000

    Delete ui.js

--- ui.js
+++
@@ -1,9 +0,0 @@
-const{ActionRowBuilder,ButtonBuilder,ButtonStyle,UserSelectMenuBuilder,StringSelectMenuBuilder}=require("discord.js");
-module.exports={buildPanelComponents:(g,id,p)=>[
-new ActionRowBuilder().addComponents(new UserSelectMenuBuilder().setCustomId(`srp:${id}:user`)),
-new ActionRowBuilder().addComponents(new StringSelectMenuBuilder().setCustomId(`srp:${id}:role`).addOptions(
-(p.allowedRoleIds||[]).slice(0,25).map(r=>({label:g.roles.cache.get(r)?.name||"role",value:r})) )),
-new ActionRowBuilder().addComponents(
-new ButtonBuilder().setCustomId(`srp:${id}:assign`).setLabel("Assign").setStyle(ButtonStyle.Success),
-new ButtonBuilder().setCustomId(`srp:${id}:remove`).setLabel("Remove").setStyle(ButtonStyle.Danger))
-]};
