ce7224703a115cc6bf554d6a7f67a2743e2476f0
JXMIE95 <jamie.clark95@googlemail.com>
2026-01-22 05:35:55 +0000

    Delete permissions.js

--- permissions.js
+++
@@ -1,9 +0,0 @@
-const {PermissionFlagsBits}=require("discord.js");
-module.exports={
-canUsePanel:(m,p)=>!p.requireManageRoles||m.permissions.has(PermissionFlagsBits.ManageRoles),
-hierarchyOk:({guild,staffMember,role})=>{
-const bot=guild.members.me;
-if(bot.roles.highest.position<=role.position)return"Bot role too low.";
-if(staffMember.roles.highest.position<=role.position)return"Your role too low.";
-return null;}
-};
